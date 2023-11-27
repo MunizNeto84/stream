f93bc07ba96aee300530bacb8de76618a9e37281
MunizNeto84 <muniznetto@outlook.com>
2023-11-26 22:39:27 -0300

    Add dependencias e constuindo o primeiro metodo get

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/videos', async (req, res) => {
+    try {
+        const [rows, fields] = await pool.query('SELECT * FROM videos');
+        res.json(rows);
+    } catch (error) {
+        console.error('Erro ao buscar dados do MySQL:', error);
+        res.status(500).send('Erro interno do servidor');
+    }
 });
+
+app.get('/videos/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const [rows, fields] = await pool.query('SELECT * FROM videos WHERE id = ?', [id]);
+
+        if (rows.length > 0) {
+            res.json(rows[0]);
+        } else {
+            res.status(404).send('Video não encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar video por id:', error);
+        res.status(500).send('Erro interno do servidor');
+    }
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
